57173f9e9ecda3674f8721b76c23afd612231242
JM Versteeg <janmerijnversteeg@gmail.com>
2016-06-13 12:30:17 +0200

    Add entrypoint

---
+++ index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+module.exports = {
+    Action:        require('./lib/Action'),
+    Context:       require('./lib/Context'),
+    GracefulError: require('./lib/GracefulError'),
+    Option:        require('./lib/Option'),
+    Reporter:      require('./lib/Reporter')
+};
