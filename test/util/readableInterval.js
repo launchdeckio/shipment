9df598a723177fb855be3a5548e9ed80fc7be743
JM Versteeg <janmerijnversteeg@gmail.com>
2016-08-09 11:24:47 +0200

    Add tests for readable-interval

---
+++ test/util/readableInterval.js
@@ -0,0 +1,21 @@
+'use strict';
+
+require('./../support/support');
+
+const ri = require('./../../lib/util/readableInterval');
+
+describe('readableInterval', () => {
+
+    it('should format the given amount of milliseconds into a more human-readable format', () => {
+
+        ri(1000).should.equal('1 s');
+        ri(2000).should.equal('2 s');
+        ri(200).should.equal('200 ms');
+        ri(875).should.equal('875 ms');
+        ri(1200).should.equal('1.2 s');
+        ri(1750).should.equal('1.75 s');
+        ri(2890).should.equal('2.9 s');
+        ri(34120).should.equal('34.1 s');
+        ri(72451).should.equal('0:01:12');
+    });
+});
