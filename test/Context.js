d2770208914a8c4fc107d88fa32c90a3dc9e8204
JM Versteeg <janmerijnversteeg@gmail.com>
2016-08-09 11:43:23 +0200

    Add full test suite for the Context class

---
+++ test/Context.js
@@ -0,0 +1,62 @@
+'use strict';
+
+require('./support/support');
+
+const Promise = require('bluebird');
+const sinon   = require('sinon');
+
+const Context  = require('./../lib/Context');
+const Reporter = require('./../lib/Reporter');
+
+describe('Context', () => {
+
+    let context,
+        CustomReporter, customReporterSpy,
+        CustomReporterContext, customReporterContext;
+
+    beforeEach(() => {
+        context                        = new Context;
+        customReporterSpy              = sinon.spy();
+        CustomReporter                 = class {
+            constructor() {
+                customReporterSpy(...arguments);
+            }
+        };
+        CustomReporterContext          = class extends Context {
+        };
+        CustomReporterContext.Reporter = CustomReporter;
+        customReporterContext          = new CustomReporterContext;
+    });
+
+    describe('makeReporter', () => {
+
+        it('should make an instance of Reporter', () => {
+
+            context.makeReporter().should.be.an.instanceOf(Reporter);
+        });
+
+        it('should allow the use of a custom Reporter class', () => {
+
+            customReporterContext.makeReporter().should.be.an.instanceOf(CustomReporter);
+        });
+
+        it('should invoke the Reporter constructor with itself as the argument', () => {
+
+            customReporterContext.makeReporter();
+            customReporterSpy.should.have.been.calledWith(customReporterContext);
+        });
+    });
+
+    describe('getUptime', () => {
+
+        it('should return the number of milliseconds that have passed since the instantiation of the context', () => {
+
+            context = new Context;
+
+            Promise.delay(20).then(() => {
+
+                context.getUptime().should.be.within(18, 22);
+            });
+        });
+    });
+});
