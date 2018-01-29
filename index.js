69ecf4f4b9388f98fc57d13c8e1a1a63d6f900fd
JM Versteeg <janmerijnversteeg@gmail.com>
2018-01-29 01:37:25 +0100

    expose stock events

--- index.js
+++ index.js
@@ -2,14 +2,14 @@
 
 const Shipment = require('./lib/Shipment');
 
-const makeApi = require('./lib/api/makeApi');
-
+const makeApi  = require('./lib/api/makeApi');
+const events   = require('./lib/events');
 const receiver = require('./lib/receive');
 
 module.exports = Shipment;
 
-module.exports.makeApi = makeApi;
-
+module.exports.makeApi  = makeApi;
+module.exports.events   = events;
 module.exports.receiver = receiver;
 
 
