aa2636d4282fce21bd4ffe9601540a09e30d0765
JM Versteeg <janmerijnversteeg@gmail.com>
2018-01-29 14:19:56 +0100

    more work to the reship version

--- index.js
+++ index.js
@@ -3,11 +3,13 @@
 const Shipment = require('./lib/Shipment');
 
 const makeApi  = require('./lib/api/makeApi');
+const makeHttp = require('./lib/http/makeHttp');
 const events   = require('./lib/events');
 const receiver = require('./lib/receive');
 
 module.exports = Shipment;
 
 module.exports.makeApi  = makeApi;
+module.exports.makeHttp = makeHttp;
 module.exports.events   = events;
 module.exports.receiver = receiver;
